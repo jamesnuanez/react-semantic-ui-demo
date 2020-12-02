3235dba1bc921beda48cb5dd3ded6a4fa92eec29
James Nuanez <james@jamesnuanez.com>
2020-12-02 12:52:58 -0800

    Remove unused import

--- src/internal/AccountHome.js
+++ src/internal/AccountHome.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { Header, Segment, Icon } from 'semantic-ui-react';
 
 class AccountHome extends Component {
